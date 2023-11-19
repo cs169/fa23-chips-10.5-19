2e4e0f17d1d78ddeef5f0d26e425a62608461962
Henley <henley@berkeley.edu>
2023-11-18 21:33:35 -0800

    counties

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -20,9 +20,11 @@ RSpec.describe Event, type: :model do
     
     describe '#county_names_by_id' do
         it 'should return a hash of county names by id' do
-            state = State.create(name: 'State', symbol: 'ST')
-            county1 = County.create(state: state, name: 'County 1')
-            county2 = County.create(state: state, name: 'County 2')
+            dummyDate = DateTime.new(2001,2,3,4,5,6)
+            state = State.create(symbol: 'test_state', symbol: 'test_state', fips_code: 'test_state', is_territory: 'test_state', lat_min: 'test_state', lat_max: 'test_state', long_min: 'test_state', long_max: 'test_state' )
+            county1 = County.create(state_id: @state.id, name: 'test_county', fips_code: 'test_county', fips_class: 'test_county', created_at: dummyDate, updated_at: dummyDate)
+            county2 = County.create(state_id: @state.id, name: 'test_county2', fips_code: 'test_county2', fips_class: 'test_county2', created_at: dummyDate, updated_at: dummyDate)
+
             event = Event.create(county: county1)
 
             expect(event.county_names_by_id).to eq({ 'County 1' => county1.id, 'County 2' => county2.id })
