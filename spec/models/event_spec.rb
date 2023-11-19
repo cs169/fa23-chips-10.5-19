e46b1b45f402848d081d543ad6004fc6e5559a58
Henley <henley@berkeley.edu>
2023-11-18 20:41:47 -0800

    events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
     describe 'validations' do
-        it { should validate_presence_of(:start_time) }
-        it { should validate_presence_of(:end_time) }
         
         it 'should validate that start_time is after today' do
             event = Event.new(start_time: Time.zone.now - 1.day)
@@ -18,24 +16,21 @@ RSpec.describe Event, type: :model do
         end
     end
     
-    describe 'associations' do
-        it { should belong_to(:county) }
-        it { should delegate_method(:state).to(:county).allow_nil }
-    end
+
     
     describe '#county_names_by_id' do
         it 'should return a hash of county names by id' do
-            state = FactoryBot.create(:state)
-            county1 = FactoryBot.create(:county, state: state, name: 'County 1')
-            county2 = FactoryBot.create(:county, state: state, name: 'County 2')
-            event = FactoryBot.create(:event, county: county1)
-            
+            state = State.create(name: 'State')
+            county1 = County.create(state: state, name: 'County 1')
+            county2 = County.create(state: state, name: 'County 2')
+            event = Event.create(county: county1)
+
             expect(event.county_names_by_id).to eq({ 'County 1' => county1.id, 'County 2' => county2.id })
         end
-        
+
         it 'should return an empty hash if county is nil' do
-            event = FactoryBot.create(:event, county: nil)
-            
+            event = Event.create(county: nil)
+
             expect(event.county_names_by_id).to eq({})
         end
     end
