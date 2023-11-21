1ee2fd3ca8668cfaafd644b1146947d97a3462f9
Kyle Chiu <kylechiu@berkeley.edu>
2023-11-21 06:16:31 +0000

    Representatives model API fetching and Record Creation Updated

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -10,6 +10,22 @@ class Representative < ApplicationRecord
       ocdid_temp = ''
       title_temp = ''
 
+      political_party_temp = official.party || ''
+      photo_temp = official.photoUrl || ''
+
+      if official.addresses.present?
+        address = official.address.first
+        street_temp = address.line1
+        city_temp = address.city
+        state_temp = address.state
+        zip_temp = address.zip
+      else
+        street_temp = ''
+        city_temp = ''
+        state_temp = ''
+        zip_temp = ''
+      end
+
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
@@ -17,8 +33,15 @@ class Representative < ApplicationRecord
         end
       end
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+      rep = Representative.create!({ name: official.name, 
+        ocdid: ocdid_temp,
+        title: title_temp,
+        street: street_temp,
+        city: city_temp,
+        state: state_temp,
+        zip: zip_temp,
+        party: political_party_temp,
+        photo: photo_temp})
       reps.push(rep)
     end
 
