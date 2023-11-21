bd0f3b1475633c9cc7fb0fbd369116086eac718f
Kyle Chiu <kylechiu@berkeley.edu>
2023-11-21 05:26:02 +0000

    Updated DB Schema Migration

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,7 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
+  def show
+    @representative_show = Representative.find(params[:id])
 end
