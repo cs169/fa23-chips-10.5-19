170d21f1bb96641c73fcf734b43762ccab12c6d9
Henley <henley@berkeley.edu>
2023-11-22 17:10:21 -0800

    Revert "Merge branch 'master' of github.com:cs169/fa23-chips-10.5-19 into kylepikachiu"
    This reverts commit d86bc4f8cc0c6ea0b2937ba3cf58ae176f6e68a8, reversing
    changes made to 0a2910cb59851a9f74233717a03b53b89b5ab0e2.

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -14,17 +14,40 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      # may need to search by more attributes
       existing_rep = Representative.find_or_initialize_by(
         name:  official.name,
         ocdid: ocdid_temp,
         title: title_temp
       )
       unless existing_rep.persisted?
-        existing_rep.attributes = { name: official.name, ocdid: ocdid_temp, title: title_temp }
+        existing_rep.attributes = create_representative(official, ocdid_temp, title_temp)
         existing_rep.save!
       end
       reps.push(existing_rep)
     end
     reps
+  end
+
+  def self.create_representative(official, ocdid_temp, title_temp)
+    address = address_hash(official)
+    { name:            official.name,
+      ocdid:           ocdid_temp,
+      title:           title_temp,
+      street:          address[0],
+      city:            address[1],
+      state:           address[2],
+      zip:             address[3],
+      political_party: official.party || '',
+      photo:           official.photoUrl || '' }
+  end
+
+  def self.address_hash(official)
+    if official.address.present?
+      address = official.address.first
+      [address.line1, address.city, address.state, address.zip]
+    else
+      ['', '', '', '']
+    end
   end
 end
