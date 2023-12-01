ac36ae9bbebc22533c9754b5a46f4e01559983ce
Ryan Mathew <ryanmathew@berkeley.edu>
2023-12-01 23:56:16 +0000

    fixed testing issues / lint issues

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -1,44 +1,8 @@
 # frozen_string_literal: true
 
-# # frozen_string_literal: true
-# # # frozen_string_literal: true
-
-# require 'rails_helper'
-
-# RSpec.describe NewsItem, type: :model do
-#   describe 'class methods' do
-#     describe '.find_for' do
-#       let(:representative) { double('Representative', id: 1) } # Using a double for Representative
-#       let!(:news_item) { double('NewsItem') } # Using a double for NewsItem
-
-#       before do
-#         allow(NewsItem).to receive(:create).and_return(news_item)
-#         allow(news_item).to receive(:representative).and_return(representative)
-#       end
-
-#       it 'returns the news item for the given representative id' do
-#         found_news_item = described_class.find_for(representative.id)
-#         expect(found_news_item).to eq(news_item)
-#       end
-#     end
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe NewsItem, type: :model do
-  # let(:issues_list) do
-  #   ['Free Speech', 'Immigration', 'Terrorism',
-  #    'Social Security and Medicare', 'Abortion',
-  #    'Student Loans', 'Gun Control', 'Unemployment',
-  #    'Climate Change', 'Homelessness', 'Racism',
-  #    'Tax Reform', 'Net Neutrality', 'Religious Freedom',
-  #    'Border Security', 'Minimum Wage', 'Equal Pay']
-  # end
-
-  # it 'has a valid list of issues' do
-  #   expect(NewsItem::ISSUES_LIST).to match_array(issues_list)
-  # end
-
   it 'validates presence of issue' do
     news_article = described_class.new(issue: nil)
     expect(news_article).not_to be_valid
