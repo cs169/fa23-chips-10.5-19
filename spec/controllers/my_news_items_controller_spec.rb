deb0be70d0bf2ec33087238cc07bfbfcb8a46d98
Henley Zhang <henley@berkeley.edu>
2023-12-02 00:32:38 +0000

    add twest

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+RSpec.describe MyNewsItemsController, type: :controller do
+  describe '#search_news_items' do
+    let(:representative) { Representative.create }
+    let(:issue) { 'your_issue' }
+    let(:params) do
+      { representative_id: representative.id, news_item: { representative_id: representative.id, issue: issue } }
+    end
+
+    it 'assigns the representative' do
+      post :search_news_items, params: params
+      expect(assigns(:representative)).to eq(@representative)
+    end
+
+    it 'assigns the selected issue' do
+      post :search_news_items, params: params
+      expect(assigns(:selected_issue)).to eq(@issue)
+    end
+  end
+end
